cb61d62b6fafa2403c826b665e396d0bc84ceb17
minhtriizkooooo-sys <minhtriizkooooo@gmail.com>
2026-01-25 12:56:31 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from flask_socketio import SocketIO, emit, join_room
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ai_quiz_ultra_2026'
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 # Quản lý trạng thái trò chơi
 game_state = {
@@ -145,3 +145,4 @@ def update_lb():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
+
