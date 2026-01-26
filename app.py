45b2ae41d3d64e29e68dd4c44ff78cfd60dc27c6
minhtriizkooooo-sys <minhtriizkooooo@gmail.com>
2026-01-27 00:26:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import gevent.monkey
 gevent.monkey.patch_all()
 
 import os, random, qrcode, io, base64, time, pandas as pd
@@ -198,3 +199,4 @@ def get_host_review():
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
+
